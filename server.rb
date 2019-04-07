b51ad8fcee7722a665c2820b9463c264cf8f2e4e
Ankit Bahl <ankit2bahl@gmail.com>
2019-04-07 10:25:38 -0400

    add ssl support

--- server.rb
+++ server.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
 require 'sinatra/cors'
 require 'combine_pdf'
+require './sinatra_ssl'
 
 set :allow_origin, 'https://ankitbahl.github.io/'
 set :allow_methods, 'GET,HEAD,POST, OPTIONS'
 set :allow_headers, 'content-type,if-modified-since'
 set :expose_headers, 'location,link'
+set :ssl_certificate, 'cert.crt'
+set :ssl_key, 'pkey.pem'
+set :port, 4567
 
 post '/merge' do
 	File.open('files.zip', 'w') do |f|
